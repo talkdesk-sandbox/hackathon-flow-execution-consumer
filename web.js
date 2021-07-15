7de17912ab9d01b836edefe883ca61dba3af6ef0
Pedro Piloto <pedro@pedropiloto.com>
2021-07-15 16:05:07 +0100

    cors cenas

--- web.js
+++ web.js
@@ -5,6 +5,12 @@ const app = express();
 const FetchExecutionByAccountInteractor = require('./interactors/fetch_execution_by_account_interactor')
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/flow_execution", async function(req, res) {
   const account_id = req.query.account_id
   if(!account_id){
