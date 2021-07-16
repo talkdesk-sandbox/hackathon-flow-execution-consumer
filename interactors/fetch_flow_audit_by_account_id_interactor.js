689c83451bfcc235d5dc0e7976ebb6a6b18b219a
Pedro Piloto <pedro@pedropiloto.com>
2021-07-16 17:16:21 +0100

    cenas

--- interactors/fetch_flow_audit_by_account_id_interactor.js
+++ interactors/fetch_flow_audit_by_account_id_interactor.js
@@ -3,7 +3,7 @@ const FlowAudit = require("../models/flow_audit");
 const call = async (account_id) => {
   rawData = await FlowAudit.find({
     account_id: account_id,
-  }).limit(10);
+  }).sort({'timestamp': 'asc'}).limit(10);
 
   return rawData;
 };
